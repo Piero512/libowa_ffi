a974c2e29e91fc945d008993c1ca89eb11b36e9e
Piero512 <6634316+Piero512@users.noreply.github.com>
2023-10-20 07:44:47 -0500

    Compile experimental APIs, needed for seeking the audio file

--- src/oswrapper_audio.c
+++ src/oswrapper_audio.c
@@ -3,6 +3,7 @@
 #define OSWRAPPER_AUDIO_DEF __declspec( dllexport )
 #endif
 #include <stdbool.h>
+#define OSWRAPPER_AUDIO_EXPERIMENTAL
 #define OSWRAPPER_AUDIO_RESULT_TYPE bool
 #define OSWRAPPER_AUDIO_RESULT_SUCCESS true
 #define OSWRAPPER_AUDIO_RESULT_FAILURE false
